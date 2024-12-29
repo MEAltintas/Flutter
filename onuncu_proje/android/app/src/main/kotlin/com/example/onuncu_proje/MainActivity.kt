944e10e5a0a943260f610f3871976c5e68c34b95
Münüre Ezgi <mezgialtintas@gmail.com>
2024-12-29 15:14:45 +0300

    Ürünler

---
+++ onuncu_proje/android/app/src/main/kotlin/com/example/onuncu_proje/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.onuncu_proje
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ onuncu_proje/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:onuncu_proje/screens/home_screen.dart';
+
+
+void main() {
+  runApp(const MyWidget());
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      //telefoda kçşede olan kırmızı etiketi kaldırır
+      theme: ThemeData(fontFamily: "Nunito"),
+      home: const HomeScreen(),
+    );
+  }
+}
